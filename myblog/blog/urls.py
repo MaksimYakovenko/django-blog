dc8654c0ebeaa1a2fd281f52e89deef570ad73f1
MaksimYakovenko <maksim13yakovenko@gmail.com>
2024-01-08 01:19:30 +0200

    Initial Commit

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (HomeView, ArticleDetailView, AddPostView, UpdatePostView,
                     DeletePostView, AddCategoryView, CategoryView,
-                    CategoryListView, LikeView)
+                    CategoryListView, LikeView, AddCommentView)
 
 urlpatterns = [
     # path('', views.home, name='home'),
@@ -16,4 +16,6 @@ urlpatterns = [
     path('category/<str:cats>/', CategoryView, name='category'),
     path('category-list/', CategoryListView, name='category-list'),
     path('like/<int:pk>', LikeView, name='like_post'),
+    path('article/<int:pk>/comment/', AddCommentView.as_view(),
+         name='add_comment')
 ]
